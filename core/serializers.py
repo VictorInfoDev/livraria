e863cab5ed0b1d0103f30f5e9c1e15ec41de1bf6
VictorInfoDev <victorhugoagostini777@gmail.com>
2022-07-12 09:06:48 -0300

    commit Inicial

--- core/serializers.py
+++ core/serializers.py
@@ -1,1 +1,9 @@
+from turtle import mode
 from rest_framework.serializers import ModelSerializer
+
+from core.models import Categoria
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta: 
+        model = Categoria
+        fields = '__all__'
